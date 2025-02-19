b0114679ddad6bef9c478134483722c68cc9694e
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2025-02-19 12:42:14 +0100

    fix: create store for cleaning parameters and update form components

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -1,6 +1,7 @@
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa6';
 
 const Carousel = () => {
   const images = [
